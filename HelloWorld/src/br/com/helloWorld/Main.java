9aab3567394d701deca5071a478379ef1a2f98de
Arthur <arthursantosgoncalves587@gmail.com>
2024-03-01 22:08:13 -0300

    second commit

---
+++ HelloWorld/src/br/com/helloWorld/Main.java
@@ -0,0 +1,19 @@
+package br.com.helloWorld;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		String nome = "Arthur";
+		Integer codigo = 9;
+
+		boolean status = true;
+		
+		System.out.println(String.format("Código: %d \nNome: %s", codigo, nome));
+		
+		
+
+	}
+	
+
+}
